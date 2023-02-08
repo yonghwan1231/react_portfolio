60df0cb049a97dbeb1cb9baed370654598d9b9cf
yonghwanjung <yhjung1231@bythem.net>
2023-02-09 01:55:01 +0900

    230209 페이지네이션 버튼태그 수정(a->Link)

--- src/components/PageNation.js
+++ src/components/PageNation.js
@@ -1,21 +1,23 @@
+import { Link } from 'react-router-dom'
+
 export function PageNation(props) {
   return (
     <section className="pagination">
-      <a href={'#' + (props.currentPage)} className="prev-page" onClick={() => {
+      <Link to={'#' + (props.currentPage)} className="prev-page" onClick={() => {
         if (props.currentPage != 1) props.setCurrentPage(props.currentPage - 1)
-      }}>&lt;</a>
+      }}>&lt;</Link>
       {
         props.pages.map((el, idx) => {
           return (
-            <a href={'#' + (idx + 1)} key={idx} className={props.currentPage === el ? 'page-num active' : 'page-num'} onClick={() => {
+            <Link to={'#' + (idx + 1)} key={idx} className={props.currentPage === el ? 'page-num active' : 'page-num'} onClick={() => {
               props.setCurrentPage(idx + 1)
-            }}>{el}</a>
+            }}>{el}</Link>
           )
         })
       }
-      <a href={'#' + (props.currentPage)} className="next-page" onClick={() => {
+      <Link to={'#' + (props.currentPage)} className="next-page" onClick={() => {
         if (props.pages.length > props.currentPage) props.setCurrentPage(props.currentPage + 1)
-      }}>&gt;</a>
+      }}>&gt;</Link>
     </section>
   )
 }
