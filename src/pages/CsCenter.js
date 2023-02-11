29a1e42a6f3971bead45f62daf93abbbf79cec59
yonghwanjung <yhjung1231@bythem.net>
2023-02-11 14:30:54 +0900

    230211 불필요파일 삭제, 고객센터페이지 axios url 오타 수정

--- src/pages/CsCenter.js
+++ src/pages/CsCenter.js
@@ -11,7 +11,7 @@ export function CsCenter() {
 
   useEffect(() => {
     axios({
-      url: 'http://localhost:8080/api/accesstoken',
+      url: 'https://port-0-portfolio-server-private-4y6tt2blds7g9x0.sel3.cloudtype.app/api/accesstoken',
       method: 'GET',
       withCredentials: true
     })
