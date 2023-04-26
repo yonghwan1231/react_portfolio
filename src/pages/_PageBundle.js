cb190dd6dce1cc82db8ad999af696336df19a8e7
yonghwan <coxldtpeo1@naver.com>
2023-04-26 10:32:30 +0900

    230426 청크파일 생성

--- src/pages/_PageBundle.js
+++ src/pages/_PageBundle.js
@@ -1,33 +1,17 @@
-import { Main } from './Main'
-import { Event } from './Event'
-import { LookBook } from './LookBook'
-import { Login } from './Login'
-import { Join } from './Join'
-import { Mypage } from './Mypage'
-import { Product } from './Product'
-import { ProductDetail } from './ProductDetail'
-import { MyCart } from './MyCart'
-import { MyLike } from './MyLike'
-import { MyWatched } from './MyWatched'
-import { Payment } from './Payment'
-import { CsCenter } from './CsCenter'
-import { CsWrite } from './CsWrite'
-import { CsHistory } from './CsHistory'
+import { Main } from "./Main";
+import { Event } from "./Event";
+import { LookBook } from "./LookBook";
+import { Login } from "./Login";
+import { Join } from "./Join";
+import { Mypage } from "./Mypage";
+// import Product from "./Product";
+import { ProductDetail } from "./ProductDetail";
+import { MyCart } from "./MyCart";
+import { MyLike } from "./MyLike";
+import { MyWatched } from "./MyWatched";
+import { Payment } from "./Payment";
+import { CsCenter } from "./CsCenter";
+import { CsWrite } from "./CsWrite";
+import { CsHistory } from "./CsHistory";
 
-export {
-  Main,
-  Event,
-  LookBook,
-  Login,
-  Join,
-  Mypage,
-  Product,
-  ProductDetail,
-  MyCart,
-  MyLike,
-  MyWatched,
-  Payment,
-  CsCenter,
-  CsWrite,
-  CsHistory
-}
+export { Main, Event, LookBook, Login, Join, Mypage, ProductDetail, MyCart, MyLike, MyWatched, Payment, CsCenter, CsWrite, CsHistory };
